e5970a949528002078267d46d72a9cf49ef6fc00
Ilies-Bouderba <bouderbailies2@gmail.com>
2025-01-17 01:16:12 +0100

    Add models, factories, and migrations for Category, Comment, and Post as well as Seeders for my db

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Category;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->sentence,
+            "content" => $this->faker->paragraph,
+            "image" => $this->faker->imageUrl,
+            "user_id" => User::inRandomOrder()->first()->id,
+            "category_id" => Category::inRandomOrder()->first()->id,
+
+        ];
+    }
+}
